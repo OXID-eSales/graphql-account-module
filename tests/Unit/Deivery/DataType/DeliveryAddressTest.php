22685a4f132eb6591304987d4ab808114574dc0a
j.skoczek <j.skoczek@createit.pl>
2020-09-18 10:32:26 +0200

    fixup

--- tests/Unit/Deivery/DataType/DeliveryAddressTest.php
+++ tests/Unit/Deivery/DataType/DeliveryAddressTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace OxidEsales\GraphQL\Account\Tests\Unit\Delivery\DataType;
 
 use OxidEsales\Eshop\Application\Model\Address as EshopAddressModel;
-use OxidEsales\GraphQL\Account\Account\DataType\DeliveryAddress;
+use OxidEsales\GraphQL\Account\Delivery\DataType\DeliveryAddress;
 use PHPUnit\Framework\Constraint\IsType;
 use PHPUnit\Framework\TestCase;
 
