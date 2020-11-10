3d630c60adea4fa2e135cdacaf7e65f49cbd340c
Aneliya Lambreva <aneliya.lambreva@dnhsoft.com>
2020-11-10 17:16:44 +0200

    OXDEV-4100 Extend voucher model and use in it graphql basket model

--- src/Shared/Shop/Voucher.php
+++ src/Shared/Shop/Voucher.php
@@ -9,6 +9,10 @@ declare(strict_types=1);
 
 namespace OxidEsales\GraphQL\Account\Shared\Shop;
 
+use OxidEsales\Eshop\Application\Model\Basket as EshopBasketModel;
+use OxidEsales\GraphQL\Account\Basket\Service\Basket as BasketService;
+use OxidEsales\GraphQL\Account\Shared\Infrastructure\Basket as SharedBasketInfrastructure;
+
 /**
  * Voucher model extended
  *
@@ -33,5 +37,59 @@ class Voucher extends Voucher_parent
             );
             $this->save();
         }
+    }
+
+    protected function _getBasketItems($oDiscount = null): array
+    {
+        $items = parent::_getBasketItems($oDiscount);
+        if (empty($items)) {
+            $items = $this->getGraphQLBasketItems($oDiscount);
+        }
+
+        return $items;
+    }
+
+    protected function getGraphQLBasketItems($oDiscount = null): array
+    {
+        if (is_null($oDiscount)) {
+            $oDiscount = $this->_getSerieDiscount();
+        }
+
+        $oBasket = $this->getGraphQLBasket();
+        $aItems = [];
+        $iCount = 0;
+
+        foreach ($oBasket->getContents() as $oBasketItem) {
+            if (!$oBasketItem->isDiscountArticle() && ($oArticle = $oBasketItem->getArticle()) && !$oArticle->skipDiscounts() && $oDiscount->isForBasketItem($oArticle)) {
+                $aItems[$iCount] = [
+                    'oxid'     => $oArticle->getId(),
+                    'price'    => $oArticle->getBasketPrice($oBasketItem->getAmount(), $oBasketItem->getSelList(), $oBasket)->getPrice(),
+                    'discount' => $oDiscount->getAbsValue($oArticle->getBasketPrice($oBasketItem->getAmount(), $oBasketItem->getSelList(), $oBasket)->getPrice()),
+                    'amount'   => $oBasketItem->getAmount(),
+                ];
+
+                $iCount++;
+            }
+        }
+
+        return $aItems;
+    }
+
+    protected function getGraphQLBasket(): ?EshopBasketModel
+    {
+        $basketModel = null;
+        $basketId    = $this->getFieldData('oegql_basketid');
+
+        if ($basketId) {
+            /** @var BasketService $basketService */
+            $basketService = $this->getContainer()->get(BasketService::class);
+            $basket = $basketService->basket($basketId);
+
+            /** @var SharedBasketInfrastructure $sharedBasketInfrastructure */
+            $sharedBasketInfrastructure = $this->getContainer()->get(SharedBasketInfrastructure::class);
+            $basketModel = $sharedBasketInfrastructure->getBasket($basket);
+        }
+
+        return $basketModel;
     }
 }
